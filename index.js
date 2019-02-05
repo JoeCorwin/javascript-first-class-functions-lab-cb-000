2f1127b5270c837692a36838d2b7fadf3d2597ba
Joe Corwin <joe.corwin@gmail.com>
2019-02-05 23:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function countdown (callback) {
   window.setTimeout(callback, 2000);
 }
 
-function createMultiplier(multiplierValue,n) {
-  var num = Number(n);
-  var mult = Number(multiplierValue);
-  return () => num * mult
-}
+// function createMultiplier(multiplierValue,n) {
+//   var num = Number(n);
+//   var mult = Number(multiplierValue);
+//   return () => num * mult
+// }
 
 
 function createMultiplier(multiplierValue,n) {
