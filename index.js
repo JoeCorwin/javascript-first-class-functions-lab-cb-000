31dd6af1a79d5aece36799629ccb30d61f187d0b
Joe Corwin <joe.corwin@gmail.com>
2019-02-06 00:20:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ var tripler = createMultiplier(3);
 
 
 function multiplier(n1, n2) {
-return n * i
-
+  return n * i
 }
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
